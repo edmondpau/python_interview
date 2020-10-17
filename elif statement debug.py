f227b7c05309e9698c1797f04a29fb0dae912f87
chunlok pau <edmondpau@g.ucla.edu>
2020-10-16 21:19:32 -0700

    elif indentation bug

---
+++ elif statement debug.py
@@ -0,0 +1,23 @@
+is_dick=False
+is_pussy=False
+if is_dick and is_pussy:
+    print("Get fk")
+elif is_dick and not (is_pussy):
+    print("WOWOOWO")
+elif not (is_dick) and (is_pussy):
+    print("minus plus")
+else:
+    print("WTF")
+
+
+a = False
+b = False
+
+if a and b:
+    print("positive positive")
+elif not (a) and (b):
+    print("only b true")
+elif a and not (b):
+    print("only a positive")
+else:
+    print("both false")
